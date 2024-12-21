e3176e6a5dd0b8ae57f30aac43ea5c4c8178ee14
Azharul Habib <azharulhabib16@gmail.com>
2024-12-22 03:03:32 +0600

    login interface created

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -1,35 +1,43 @@
 'use client';
 
 import { useActionState } from 'react';
-// import { classes } from './page.module.css';
+import classes from './page.module.css';
 import { LoginButton } from '@/components/button';
 import { loginUser } from "@/utils/actions";
+import Image from 'next/image';
+import logoImg from '@/icons/circle_design.svg';
+
 
 export default function loginPage () {
   const [state, formAction] = useActionState(loginUser, { errors: null });
 
   return (
-    <div>
-      <header>
-        <h1>Login</h1>
-      </header>
-      <main>
-        <form action={formAction}>
-          {state.errors && <p>{state.errors}</p>}
-          {/* {state.errors && (
-            <ErrorAlert message={state.errors} onClose={() => {}} />
-          )} */}
-          <p>
-            <label htmlFor="email">Email:</label>
-            <input type="text" id="email" name="email" required />
-          </p>
-          <p>
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" required />
-          </p>
-          <LoginButton />
-        </form>
-      </main>
-    </div>
+    <div className={classes.login_container}>
+        <div className={classes.logo}>
+            <Image src={logoImg} className={classes.logo_icon} alt="Blog-Sphere-logo" priority />
+            <span>SPHERE</span>
+
+
+          </div>
+          <div className={classes.login_form_container}>
+            <h1>Login</h1>
+            <form action={formAction} className={classes.login_form}>
+              {state.errors && <p>{state.errors}</p>}
+              {/* {state.errors && (
+                <ErrorAlert message={state.errors} onClose={() => {}} />
+              )} */}
+              <div className= {classes.input_group}>
+                <input type="text" placeholder="EMAIL" name="email" required/>
+                <i className= {classes.user_icon}></i>
+              </div>
+
+              <div className = {classes.input_group}>
+                <input type="password" placeholder="PASSWORD" name='password' required/>
+                <i className= {classes.password_icon}></i>
+              </div>
+              <LoginButton />
+            </form>
+        </div>
+      </div>
   );
 };
--- frontend/src/components/button.js
+++ frontend/src/components/button.js
@@ -1,12 +1,13 @@
 'use client'
 
 import { useFormStatus } from "react-dom";
+import classes from "./button.module.css"
 
 export function LoginButton () {
   const { pending } = useFormStatus()
 
   return (
-    <button disabled={pending} type="submit">
+    <button disabled={pending} type="submit" className={classes.login_button} >
       {pending ? 'Logging in...' : 'Login'}
     </button>
   );
