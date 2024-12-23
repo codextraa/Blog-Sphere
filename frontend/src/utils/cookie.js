e53cf4f49c627155779f3e49a01b55c1ed54dd58
Wasee Sun <revengersun@gmail.com>
2024-12-23 14:16:22 +0600

    cookies deleted

--- frontend/src/utils/cookie.js
+++
@@ -1,6 +0,0 @@
-'use client';
-
-export function setTokencookie(access, refresh) {
-  document.cookie = `access_token=${access}; path=/sphere; SameSite=strict; Secure;`;
-  document.cookie = `refresh_token=${refresh}; path=/sphere; SameSite=strict; Secure;`;
-}
